aa362422b16530c89c30c731bcbb7bc680aef978
adilsontchameia <adilsontchameia@gmail.com>
2024-04-06 18:08:19 +0100

    feat: state provider + provider (for readyonly)

---
+++ lib/presentation/providers/04/todos_provider.g.dart
@@ -0,0 +1,55 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todos_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$filteredTodosHash() => r'5e3be705d323e756a4a34c3d5aa56b95b80146e3';
+
+/// See also [filteredTodos].
+@ProviderFor(filteredTodos)
+final filteredTodosProvider = AutoDisposeProvider<List<Todo>>.internal(
+  filteredTodos,
+  name: r'filteredTodosProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$filteredTodosHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FilteredTodosRef = AutoDisposeProviderRef<List<Todo>>;
+String _$todoCurrentFilterHash() => r'71ad0b4c662224b9e7522a7a40c0cf18af223650';
+
+/// See also [TodoCurrentFilter].
+@ProviderFor(TodoCurrentFilter)
+final todoCurrentFilterProvider =
+    NotifierProvider<TodoCurrentFilter, FilterType>.internal(
+  TodoCurrentFilter.new,
+  name: r'todoCurrentFilterProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$todoCurrentFilterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TodoCurrentFilter = Notifier<FilterType>;
+String _$todosHash() => r'fa64896c19976cb78235a8082525fe86f9c309d4';
+
+/// See also [Todos].
+@ProviderFor(Todos)
+final todosProvider = NotifierProvider<Todos, List<Todo>>.internal(
+  Todos.new,
+  name: r'todosProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$todosHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Todos = Notifier<List<Todo>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -1,2 +1,3 @@
 export '01/01_hello_world_provider.dart';
 export '03/state_providers.dart';
+export '04/todos_provider.dart';
